dd339d5d762d8b0a6bcac41e03756dcee0f68973
Kombanyt <136236366+Kombanyt@users.noreply.github.com>
2023-06-13 13:41:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,13 +10,13 @@ global.antitags = true
 //global.autoTyping = false //auto tying in gc (true to on, false to off)
 //global.autoRecord = true //auto recording (true to on, false to off)
 
-global.Owner = ['916297175943','919362639362']  //If you want singal number so global.Owner = ['916297175943'] Change into your number
-global.OwnerNumber = ['916297175943','919362639362']  //If you want singal number so global.Owner = ['916297175943'] Change into your number
+global.Owner = ['919188732616','919362639362']  //If you want singal number so global.Owner = ['916297175943'] Change into your number
+global.OwnerNumber = ['919188732616','919362639362']  //If you want singal number so global.Owner = ['916297175943'] Change into your number
 global.ownertag = ['916297175943']
 global.BotName = "A17"
-global.packname = "A17 bot"
+global.packname = "🇯🇵⃞⃯⃮⃰𝐁⃪⃪⃪⃪⃪⃪⃪𝐋 𝐓⃪𝐡⃪𝐫⃪𝐢⃪𝐥⃪𝐨⃪𝐤⃪ ✰𝐒⃪⃪⃪⃪⃪⃪⃪⃪ɪ⃪⃪⃪⃪⃪⃪⃪⃮⃑𝐑͍᛭⃞֟፝🇬🇬"
 global.author = "By: Kai"
-global.OwnerName = "Kai"
+global.OwnerName = "🇯🇵⃞⃯⃮⃰𝐁⃪⃪⃪⃪⃪⃪⃪𝐋 𝐓⃪𝐡⃪𝐫⃪𝐢⃪𝐥⃪𝐨⃪𝐤⃪ ✰𝐒⃪⃪⃪⃪⃪⃪⃪⃪ɪ⃪⃪⃪⃪⃪⃪⃪⃮⃑𝐑͍᛭⃞֟፝🇬🇬"
 global.BotSourceCode = "https://github.com/Kai0071/A17"
 global.SupportGroupLink = "https://chat.whatsapp.com/EsNqFrGKxGXFrdwgRaQl9q"
 global.sessionName = "session"
