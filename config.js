28cc21daa670a1ab1bc87c53583e5343a6ece0f0
hive-sammie <140321781+hive-sammie@users.noreply.github.com>
2023-08-03 20:02:01 +0100

    Update config.js

--- config.js
+++ config.js
@@ -15,11 +15,11 @@ global.autoRecord = false; //auto recording (true to on, false to off)
 
 
 //
-global.Owner = ["916297175943","919362639362"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number
-global.OwnerNumber = ["916297175943","919362639362"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number
-global.ownertag = ["916297175943"];
-global.OwnerName = "Kai";
-global.BotName = "A17";
+global.Owner = ["2348052661032"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number
+global.OwnerNumber = ["2348052661032"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number
+global.ownertag = ["2348052661032"];
+global.OwnerName = "sammie💱♻️💲";
+global.BotName = "🎎🎎⚔️⚔️";
 global.packname = "A17 Bot";  //Do not change.
 global.author = "By: Kai";   //Do not change.
 global.BotSourceCode = "https://github.com/Kai0071/A17";  //Do not change.
