5ddd230b81e73b0472c57a5ead52919d2909f138
black-mage-of-darkness <141626103+black-mage-of-darkness@users.noreply.github.com>
2023-08-07 14:27:40 +0000

    Update config.js

--- config.js
+++ config.js
@@ -18,10 +18,10 @@ global.autoRecord = false; //auto recording (true to on, false to off)
 global.Owner = ["916297175943","919362639362"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number
 global.OwnerNumber = ["916297175943","919362639362"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number
 global.ownertag = ["916297175943"];
-global.OwnerName = "Kai";
-global.BotName = "A17";
-global.packname = "A17 Bot";  //Do not change.
-global.author = "By: Kai";   //Do not change.
+global.OwnerName = "yami sukehiro";
+global.BotName = "Black mage of darkness";
+global.packname = "black magus Bot";  
+global.author = "By: Kai";   //
 global.BotSourceCode = "https://github.com/Kai0071/A17";  //Do not change.
 global.SupportGroupLink = "https://chat.whatsapp.com/GhRGdwfaMVDCoeAdzILfl"; 
 
