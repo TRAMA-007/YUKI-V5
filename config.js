a7378fe10e965a7d4e114a8f7eb1969beb47a74b
Suroj01 <143651704+Suroj01@users.noreply.github.com>
2023-08-31 21:36:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,16 +10,16 @@ global.antitags = true;
 
 
 //auto functioner
-global.autoTyping = false; //auto tying in gc (true to on, false to off)
-global.autoRecord = false; //auto recording (true to on, false to off)
+global.autoTyping = true; //auto tying in gc (true to on, false to off)
+global.autoRecord = true; //auto recording (true to on, false to off)
 
 
 //
-global.Owner = ["916297175943","919362639362"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number
-global.OwnerNumber = ["916297175943","919362639362"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number
-global.ownertag = ["916297175943"];
-global.OwnerName = "Kai";
-global.BotName = "A17";
+global.Owner = ["9083778342"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number
+global.OwnerNumber = ["9083778342"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number
+global.ownertag = ["9083778342"];
+global.OwnerName = "Tupai";
+global.BotName = "TBOT";
 global.packname = "A17 Bot";  //Do not change.
 global.author = "By: Kai";   //Do not change.
 global.BotSourceCode = "https://github.com/Kai0071/A17";  //Do not change.
@@ -35,7 +35,7 @@ global.openAiAPI = "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";
 
 
 //
-global.location = "Tata City, India";
+global.location = "CoochBehar, India";
 global.reactmoji = "❤️";
 global.themeemoji = "💖";
 global.vidmenu = { url: 'https://media.tenor.com/Jdu0Ov8X2sIAAAAC/A17-Bot.mp4' };
