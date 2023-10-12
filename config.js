4a7711ff2f7e4e407f01a8e67898e733b1e05d72
Akkz007 <96486603+Akkz007@users.noreply.github.com>
2023-10-12 09:05:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -17,13 +17,13 @@ global.statusseen = true;                 //This is the new variable for control
 
 
 //
-global.Owner = ["916297175943","919362639362"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["916297175943","919362639362"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["916297175943"];
+global.Owner = ["918547647032"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["918547647032"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["918547647032"];
 global.OwnerName = "Kai";
-global.BotName = "A17";
-global.packname = "A17 Bot";                             //Do not change.
-global.author = "By: Kai";                               //Do not change.
+global.BotName = "Kannan Bhai";
+global.packname = "Kannan bhaiBot";                             //Do not change.
+global.author = "By: Ad";                               //Do not change.
 global.BotSourceCode = "https://github.com/Kai0071/A17"; //Do not change.
 global.SupportGroupLink = "https://chat.whatsapp.com/GhRGdwfaMVDCoeAdzILfl"; 
 
