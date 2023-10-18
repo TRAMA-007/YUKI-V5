833650d3c8a3a1879e76eaef16cc15cce3cfc6cc
143syam <137412833+143syam@users.noreply.github.com>
2023-10-18 15:45:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -17,12 +17,12 @@ global.statusseen = true;                 //This is the new variable for control
 
 
 //
-global.Owner = ["916297175943","919362639362"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["916297175943","919362639362"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["916297175943"];
+global.Owner = ["919947121619"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["919947121619"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["919947121619"];
 global.OwnerName = "Kai";
-global.BotName = "A17";
-global.packname = "A17 Bot";                             //Do not change.
+global.BotName = "SYAM_SER";
+global.packname = "SYAM_SER";                             //Do not change.
 global.author = "By: Kai";                               //Do not change.
 global.BotSourceCode = "https://github.com/Kai0071/A17"; //Do not change.
 global.SupportGroupLink = "https://chat.whatsapp.com/GhRGdwfaMVDCoeAdzILfl"; 
