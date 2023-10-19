9cc76e80e51e27081ddaaf600d6f2d8eecdcad90
Ezenwugo Emmanuel Ifechukwu <134739434+Ultar12@users.noreply.github.com>
2023-10-19 16:14:42 +0100

    Update config.js

--- config.js
+++ config.js
@@ -10,15 +10,15 @@ global.antitags = true;
 
 
 //auto functioner
-global.autoTyping = false;                //auto tying by default off.
+global.autoTyping = true;                //auto tying by default off.
 global.autoRecord = false;                //auto recording by default off.
 global.groupevent = false;                //This is the new variable for controlling group event handling.
 global.statusseen = true;                 //This is the new variable for controlling status seen.
 
 
 //
-global.Owner = ["916297175943","919362639362"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["916297175943","919362639362"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.Owner = ["2349163916314","919362639362"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["2349163917314","919362639362"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
 global.ownertag = ["916297175943"];
 global.OwnerName = "Kai";
 global.BotName = "A17";
