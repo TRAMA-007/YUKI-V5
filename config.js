e242e7bd333fdd96f4c697cb5d6dd16178f9b5bb
ClankillerItachiUchiha <147937117+ClankillerItachiUchiha@users.noreply.github.com>
2023-11-22 19:34:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -17,19 +17,19 @@ global.autoreadgc = true;
 
 
 //
-global.prefa = ["."];                                    //Default prefix here. you can change if you want.
+global.prefa = ["*"];                                    //Default prefix here. you can change if you want.
 
 
 //
-global.Owner = ["916297175943", "919362639362"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["916297175943", "919362639362"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["916297175943"];
-global.OwnerName = "Kai";
-global.BotName = "A17";
-global.packname = "A17 Bot";                             //Do not change.
-global.author = "By: Kai";                               //Do not change.
+global.Owner = ["ITACHI-uchiha"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["919638632711"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["919638632711"];
+global.OwnerName = "Itachi-uchiha";
+global.BotName = "CLAN-KILLER";
+global.packname = "ITACHI-UCHIHA Bot"                     //Do not change.
+global.author = "By: UCHIHA";                               //Do not change.
 global.BotSourceCode = "https://github.com/Kai0071/A17"; //Do not change.
-global.SupportGroupLink = "https://chat.whatsapp.com/GhRGdwfaMVDCoeAdzILfl";
+global.SupportGroupLink = "https://chat.whatsapp.com/DtbGR5x8uXgDwqTIxuMO9w";
 
 
 //
