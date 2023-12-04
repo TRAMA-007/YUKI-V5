3492d3f860a6a767498b86cd229fdf2b30d3b805
BotForLives <147728496+BotForLives@users.noreply.github.com>
2023-12-04 18:36:34 +0200

    Update config.js

--- config.js
+++ config.js
@@ -21,10 +21,10 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 
 //
-global.Owner = ["916297175943", "919362639362"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["916297175943", "919362639362"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["916297175943"];
-global.OwnerName = "Kai";
+global.Owner = ["27767494368", "27725693138"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["27767494368", "27725693138"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["27767494368"];
+global.OwnerName = "Ave";
 global.BotName = "A17";
 global.packname = "A17 Bot";                             //Do not change.
 global.author = "By: Kai";                               //Do not change.
