2abef81b9b4a65b064a8fd8187eb8233b398e0d2
0716838981 <152783375+0716838981@users.noreply.github.com>
2023-12-06 09:33:20 +0300

    Update config.js

--- config.js
+++ config.js
@@ -21,13 +21,13 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 
 //
-global.Owner = ["916297175943", "919362639362"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["916297175943", "919362639362"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["916297175943"];
-global.OwnerName = "Kai";
+global.Owner = ["254716838981", "254716838981"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["254716838981", "254716838981"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["254716838981"];
+global.OwnerName = "Phillo⚡️";
 global.BotName = "A17";
 global.packname = "A17 Bot";                             //Do not change.
-global.author = "By: Kai";                               //Do not change.
+global.author = "By: Phillo⚡️";                               //Do not change.
 global.BotSourceCode = "https://github.com/Kai0071/A17"; //Do not change.
 global.SupportGroupLink = "https://chat.whatsapp.com/GhRGdwfaMVDCoeAdzILfl";
 
