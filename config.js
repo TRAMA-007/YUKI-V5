401e5e699cff7319b7146a7b9949fff9ab49c2b3
nimnadineth <151133357+nimnadineth@users.noreply.github.com>
2023-12-15 22:42:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -21,12 +21,12 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 
 //
-global.Owner = ["916297175943", "919362639362"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["916297175943", "919362639362"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["916297175943"];
-global.OwnerName = "Kai";
-global.BotName = "A17";
-global.packname = "A17 Bot";                             //Do not change.
+global.Owner = ["+94714383144"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["+94714383144"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["+94714383144"];
+global.OwnerName = "Nimzen";
+global.BotName = "Nimzen";
+global.packname = "NIMZEN";                             //Do not change.
 global.author = "By: Kai";                               //Do not change.
 global.BotSourceCode = "https://github.com/Kai0071/A17"; //Do not change.
 global.SupportGroupLink = "https://chat.whatsapp.com/GhRGdwfaMVDCoeAdzILfl";
