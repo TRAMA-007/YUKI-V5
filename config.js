a06af6e71a8bb04182efeb2c48f84ca3e880cc48
Miraclelem0y <166235310+Miraclelem0y@users.noreply.github.com>
2024-04-14 12:59:51 +0800

    Update config.js

--- config.js
+++ config.js
@@ -21,9 +21,9 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 
 //
-global.Owner = ["916297175943", "919362639362"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["916297175943", "919362639362"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["916297175943"];
+global.Owner = ["6288804075817"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["6288804075817"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["6288804075817"];
 global.OwnerName = "Kai";
 global.BotName = "A17";
 global.packname = "A17 Bot";                             //Do not change.
