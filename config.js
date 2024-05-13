c2f69689bd4f914f3a5c7afff8320af088dbd663
shantobiswas1100 <shantobiswas1100@gmail.com>
2024-05-13 14:31:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -21,11 +21,11 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 
 //
-global.Owner = ["916297175943", "919362639362"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["916297175943", "919362639362"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["916297175943"];
-global.OwnerName = "Kai";
-global.BotName = "A17";
+global.Owner = ["919971752696"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["919971752696", "918920659106"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["919971752696"];
+global.OwnerName = "Adi";
+global.BotName = "LUFFY";
 global.packname = "A17 Bot";                             //Do not change.
 global.author = "By: Kai";                               //Do not change.
 global.BotSourceCode = "https://github.com/Kai0071/A17"; //Do not change.
